abe7d333fc57aff56eed331bdeecaebe960747d4
ZachBenetzky <benetzkyz@etown.edu>
2026-04-26 20:50:13 -0400

    more sql

--- analytics.sql
+++ analytics.sql
@@ -6,7 +6,7 @@ Select season, avg(avg) from mlb_hitters where "atBats" > 100  group by season o
 
 Select season, avg(era::numeric) from mlb_pitchers where "inningsPitched" > 30  group by season order by season;
 
-Select season, avg("stolenBases") from mlb_hitters group by season order by season;
+Select season, avg("stolenBases") from mlb_hitters where "atBats" > 100 group by season order by season;
 
 Select season, avg("stolenBasePercentage"::numeric) from mlb_hitters group by season order by season;
 
@@ -14,3 +14,10 @@ Select season, avg(obp) from mlb_hitters where "atBats" > 30  group by season or
 
 Select season, avg("inningsPitched") from mlb_pitchers where "inningsPitched" > 30 and "gamesStarted" > 0 group by season order by season;
 
+Select season, avg("balks") from mlb_pitchers where "inningsPitched" > 30 group by season order by season;
+
+Select season, avg("strikeoutWalkRatio"::numeric) from mlb_pitchers where "inningsPitched" > 30 group by season order by season;
+
+Select season, avg("baseOnBalls") from mlb_hitters where "atBats" > 30  group by season order by season;
+
+Select season, avg("hitByPitch") from mlb_pitchers where "inningsPitched" > 30 group by season order by season;
